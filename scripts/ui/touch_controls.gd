c2774954eb8dbb300ed7f469fb8efe2a869afc85
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-06 03:35:54 +0800

    Add mobile farm tool controls

--- scripts/ui/touch_controls.gd
+++ scripts/ui/touch_controls.gd
@@ -7,9 +7,13 @@ const ACTIONS: Array[StringName] = [
 	&"move_up",
 	&"move_down",
 	&"sprint",
+	&"interact",
+	&"tool_prev",
+	&"tool_next",
 ]
 
 @onready var root: Control = %Root
+
 
 func _ready() -> void:
 	root.visible = DisplayServer.is_touchscreen_available() or OS.has_feature("mobile")
@@ -18,16 +22,23 @@ func _ready() -> void:
 	_bind_button(%UpButton, &"move_up")
 	_bind_button(%DownButton, &"move_down")
 	_bind_button(%SprintButton, &"sprint")
+	_bind_button(%ActionButton, &"interact")
+	_bind_button(%PreviousToolButton, &"tool_prev")
+	_bind_button(%NextToolButton, &"tool_next")
+
 
 func _bind_button(button: BaseButton, action: StringName) -> void:
 	button.button_down.connect(_press_action.bind(action))
 	button.button_up.connect(_release_action.bind(action))
 
+
 func _press_action(action: StringName) -> void:
 	Input.action_press(action)
 
+
 func _release_action(action: StringName) -> void:
 	Input.action_release(action)
+
 
 func _exit_tree() -> void:
 	for action in ACTIONS:
