9b54e6d0c8b4ab2a0f9f02fb76156835c944a528
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-06 03:08:36 +0800

    Add mobile touch control logic

---
+++ scripts/ui/touch_controls.gd
@@ -0,0 +1,34 @@
+class_name TouchControls
+extends CanvasLayer
+
+const ACTIONS: Array[StringName] = [
+	&"move_left",
+	&"move_right",
+	&"move_up",
+	&"move_down",
+	&"sprint",
+]
+
+@onready var root: Control = %Root
+
+func _ready() -> void:
+	root.visible = DisplayServer.is_touchscreen_available() or OS.has_feature("mobile")
+	_bind_button(%LeftButton, &"move_left")
+	_bind_button(%RightButton, &"move_right")
+	_bind_button(%UpButton, &"move_up")
+	_bind_button(%DownButton, &"move_down")
+	_bind_button(%SprintButton, &"sprint")
+
+func _bind_button(button: BaseButton, action: StringName) -> void:
+	button.button_down.connect(_press_action.bind(action))
+	button.button_up.connect(_release_action.bind(action))
+
+func _press_action(action: StringName) -> void:
+	Input.action_press(action)
+
+func _release_action(action: StringName) -> void:
+	Input.action_release(action)
+
+func _exit_tree() -> void:
+	for action in ACTIONS:
+		Input.action_release(action)
